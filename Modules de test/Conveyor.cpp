576a60594bff0c87dcd0e04509cc786ead00450d
ThomasBerthier <tberthier@gmail.com>
2022-03-18 14:25:59 +0100

    Ajout commentaires

--- Modules de test/Conveyor.cpp
+++ Modules de test/Conveyor.cpp
@@ -1,3 +1,15 @@
+//*********************************************************************************************
+//* Programme : Conveyor.cpp								Date : 18/03/2022
+//*														Dernière MAJ : 
+//*--------------------------------------------------------------------------------------------
+//*Programmeurs :	BERTHIER Thomas							Classe : BTSSN2
+//*					
+//*					 
+//*--------------------------------------------------------------------------------------------
+//*
+//* BUT : Permet d'utiliser le convoyeur et de récupérer les capteurs photoélectrique des vérins et du scanner.
+//*
+//*********************************************************************************************
 #include "Conveyor.h"
 #include <Windows.h>
 #include <stdlib.h> 
@@ -5,6 +17,7 @@
 Conveyor::Conveyor(QObject *parent)
 	: QObject(parent)
 {
+	//Initialization of the address of the modbus components 
 	conveyor = 0000;
 }
 
@@ -12,23 +25,23 @@ Conveyor::~Conveyor()
 {
 	
 }
-
+//Method to start the conveyor
 void Conveyor::startConveyor()
 {
 	ETZ512->writeSingleWordFC6(conveyor, 0000);
 	qDebug() << "tapis ON";
 }
-
+//Method to stop de conveyor
 void Conveyor::stopConveyor()
 {
 	ETZ512->writeSingleWordFC6(conveyor, 0001);
 	qDebug() << "tapis OFF";
 }
-
+//Method to ask for all sensors values
 void Conveyor::stateSensors()
 {
 }
-
+//Method to connect to the ETZ card via TCP. Reten true if the connection is successful and else if not. 
 bool Conveyor::connectToModbus() 
 {
 	QString test = "192.168.64.200";
--- Modules de test/main.cpp
+++ Modules de test/main.cpp
@@ -1,3 +1,15 @@
+//*********************************************************************************************
+//* Programme : main.cpp								Date : 18/03/2022
+//*														Dernière MAJ : 
+//*--------------------------------------------------------------------------------------------
+//*Programmeurs :	BERTHIER Thomas							Classe : BTSSN2
+//*					LARIDANT Julien
+//*					 
+//*--------------------------------------------------------------------------------------------
+//*
+//* BUT :  Faire communiquer le scanner et les composants du système TAPIRIS.
+//*
+//*********************************************************************************************
 #include <QtCore/QCoreApplication>
 #include "Conveyor.h"
 #include <Windows.h>
@@ -7,10 +19,12 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 	Conveyor * conveyor = new Conveyor();
+	//if we're not coneted we stop the program
 	if (conveyor->connectToModbus() == false) {
 		qDebug() << "Not connected";
 		return a.exec();
 	}
+	//Method to start and stop the conveyor 
 	conveyor->startConveyor();
 	Sleep(8000);
 	conveyor->stopConveyor();
