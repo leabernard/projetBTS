f1429a5417d644409ff7f026a51c2731adbf815a
julienlaridant <70692611+julienlaridant@users.noreply.github.com>
2022-03-24 11:34:37 +0100

    Add files via upload

--- Modules de test/module de test julien/readerSensor.cpp
+++ Modules de test/module de test julien/readerSensor.cpp
@@ -1,8 +1,17 @@
-#include "capteur.h"
+#include "readerSensor.h"
 #include "Reader.cpp"
 
 
 
+readerSensor::readerSensor(QObject * parent)
 
+	 : QObject(parent)
+		{
+	detectiondrug = new QSerialPort();
+	detectiondrug->setPortName("");
+	detectiondrug->setBaudRate(9600);
+		if (detectiondrug->open(QIODevice::ReadWrite)) {
 
-
+			connect(detectiondrug, SIGNAL(readyRead()), this, SLOT(receiveMessage()));
+		}
+}
