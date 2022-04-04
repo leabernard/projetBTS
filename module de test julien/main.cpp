8e3a7522620a65ef3ef12f8f2958c8280e710118
julienlaridant <70692611+julienlaridant@users.noreply.github.com>
2022-04-04 15:31:53 +0200

    Delete module de test julien directory

--- module de test julien/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QtCore/QCoreApplication>
-#include "Reader.h"
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-	Reader * scannerData = new Reader();
-    return a.exec();
-}
